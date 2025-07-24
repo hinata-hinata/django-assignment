48648734e7cf3b3aed1a4c17b38ef565e1a20be2
onohinata <h.ono.sys24@morijyobi.ac.jp>
2025-07-24 21:42:37 +0900

    自己紹介の更新機能実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
   path('signup/', views.signup, name='signup'),
   path('<str:username>/profile/', views.profile, name='profile'),
+  path('bioupdate/<str:username>/', views.bio, name='bio'),
 ]
