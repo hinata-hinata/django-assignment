190d6387a86e7a95ddc98d64d54c250fc449acb0
onohinata <h.ono.sys24@morijyobi.ac.jp>
2025-08-04 16:21:12 +0900

    いいね機能の実装

--- likes/models.py
+++ likes/models.py
@@ -6,6 +6,6 @@ class Like(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     tweet = models.ForeignKey('posts.Tweet', on_delete=models.CASCADE, related_name='likes')
     created_at = models.DateTimeField(auto_now_add=True)
-
+    like_count = models.PositiveIntegerField(default=0)
     class Meta:
         unique_together = ('user', 'tweet')
